d6e20e618f9332b129725429dc7124f0e8e216cb
YSH <sheepsh93@gmail.com>
2021-01-26 23:46:00 +0900

    App - 홈페이지 구현

--- main.js
+++ main.js
@@ -3,41 +3,65 @@ var fs = require('fs');
 var url = require('url');
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
-
+    var _url = request.url; //여기에 여러가지 데이터가 들어오는데
+    var queryData = url.parse(_url, true).query; // url에서 query데이터들을 가져옴
+    var pathname = url.parse(_url, true).pathname; // url에서 pathname을 가져온다.
     if(pathname === '/'){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-            var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-            response.writeHead(200);
-            response.end(template);
-        });
+        if(queryData.id === undefined){ //queryData의 id가 정의되어 있지 않다면..? undefined이다. 즉 undefined이면 home으로 가게한다.
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                var title = queryData.id;
+                var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
     } else {
         response.writeHead(404);
         response.end('Not found');
     }
-
-
-
 });
 app.listen(3000);
